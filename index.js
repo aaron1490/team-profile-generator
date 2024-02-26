68c180ea1492cfd42039e97a7a48eb5f4e4aac20
Aaron Armstrong <aaronarmstrong1490@gmail.com>
2024-02-26 11:59:30 +0000

    fix: added some basic starter questions for the application

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+// libraries and packages for the project
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+//  roles for the project
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Employee = require("./lib/Employee.js")
+
+// output pathing for the generated team memeber profiles
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
+// template for the HTML page
 const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+// init an empty array to store the data for the team
+const teamData = [];
+
+const questions = [
+
+    {
+        type: "input",
+        name: "name",
+        message: "What is your name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is your ID number?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email?"
+    },
+    {
+        type: "list",
+        name: "role",
+        message: "What is your role?",
+        choices: ["Manager", "Engineer", "Intern"]
+    }
+];
 
