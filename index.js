9e27346aee48a91a9421ec63e44e28732f8f145d
Aaron Armstrong <aaronarmstrong1490@gmail.com>
2024-03-01 10:30:32 +0000

    fix: completed list of questions for team manager, engineer and intern roles

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const util = require("util");
 
 //  roles for the project
 const Manager = require("./lib/Manager");
@@ -16,33 +17,106 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 // template for the HTML page
 const render = require("./src/page-template.js");
 
+// USER STORY
+// AS A manager
+// I WANT to generate a webpage that displays my team's basic info
+// SO THAT I have quick access to their emails and GitHub profiles
+
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 // init an empty array to store the data for the team
 const teamData = [];
 
-const questions = [
-
+const managerQuestions = [
+    // When a user starts the application, they're prompted to enter the team manager's:
     {
-        type: "input",
+        type: "input", // Name
         name: "name",
         message: "What is your name?"
     },
     {
-        type: "input",
+        type: "input", // Employee ID
         name: "id",
         message: "What is your ID number?"
     },
     {
-        type: "input",
+        type: "input", // Email
         name: "email",
         message: "What is your email?"
     },
     {
-        type: "list",
-        name: "role",
-        message: "What is your role?",
-        choices: ["Manager", "Engineer", "Intern"]
+        type: "input", // Office number
+        name: "officeNumber",
+        message: "What is your office number?"
     }
 ];
 
+const addMember = [
+    // When a user enters those requirements, the user is presented with a menu with the option to:
+    {
+        type: "list",
+        name: "addMember",
+        message: "What type of team member would you like to add?",
+        choices: ["Engineer", "Intern", "I don't want to add any more team members"]
+    }
+];
+
+// Add an engineer
+if (addMember === "Engineer") {
+
+}
+// Add an intern
+if (addMember === "Intern") {
+    
+}
+// Finish building the team
+if (addMember === "I don't want to add any more team members") {
+
+}
+const engineerQuestions = [
+    // When a user selects the engineer option, the user is prompted to enter the following and then taken back to the menu:
+    {
+        type: "input", // Engineer's Name
+        name: "name",
+        message: "What is the engineer's name?"
+    },
+    {
+        type: "input", // ID
+        name: "id",
+        message: "What is the engineer's ID number?"
+    },
+    {
+        type: "input", // Email
+        name: "email",
+        message: "What is the engineer's email?"
+    },
+    {
+        type: "input", // GitHub username
+        name: "github",
+        message: "What is the engineer's GitHub username?"
+    }
+];
+
+const internQuestions = [
+    // When a user selects the intern option, the user is prompted to enter the following and then taken back to the menu:
+    {
+        type: "input", // Intern's Name
+        name: "name",
+        message: "What is the intern's name?"
+    },
+    {
+        type: "input", // ID
+        name: "id",
+        message: "What is the intern's ID number?"
+    },
+    {
+        type: "input", // Email
+        name: "email",
+        message: "What is the intern's email?"
+    },
+    {
+        type: "input", // School
+        name: "school",
+        message: "What is the intern's school?"
+    }
+];
